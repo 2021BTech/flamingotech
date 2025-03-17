ee8da7bde9975ebc7fb4b67928c54d8d7a8a3cb4
daniel.becon <daniel.becon@premiumtrustbank.com>
2025-03-17 16:10:52 +0100

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,19 @@
-
-
 const Hero = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-4 p-4">
-    <img src="src/assets/img/home.jpg" className="w-full h-auto object-cover" alt="home" />
-    <img src="src/assets/img/about.jpg" className="w-full h-auto object-cover" alt="about" />
-    <img src="src/assets/img/1.jpg" className="w-full h-auto object-cover" alt="Product 1" />
-    <img src="src/assets/img/2.jpg" className="w-full h-auto object-cover" alt="Product 2" />
-    <img src="src/assets/img/3.jpg" className="w-full h-auto object-cover" alt="Product 3" />
-    <img src="src/assets/img/4.jpg" className="w-full h-auto object-cover" alt="Product 4" />
-    <img src="src/assets/img/5.jpg" className="w-full h-auto object-cover" alt="Product 5" />
-    <img src="src/assets/img/6.jpg" className="w-full h-auto object-cover" alt="Product 6" />
-    <img src="src/assets/img/7.jpg" className="w-full h-auto object-cover" alt="Product 7" />
-    <img src="src/assets/img/8.jpg" className="w-full h-auto object-cover" alt="Product 8" />
-    <img src="src/assets/img/9.jpg" className="w-full h-auto object-cover" alt="Product 9" />
-    <img src="src/assets/img/10.jpg" className="w-full h-auto object-cover" alt="Product 10" />
-  </div>
+      <img src="/img/home.jpg" className="w-full h-auto object-cover" alt="home" />
+      <img src="/img/about.jpg" className="w-full h-auto object-cover" alt="about" />
+      <img src="/img/1.jpg" className="w-full h-auto object-cover" alt="Product 1" />
+      <img src="/img/2.jpg" className="w-full h-auto object-cover" alt="Product 2" />
+      <img src="/img/3.jpg" className="w-full h-auto object-cover" alt="Product 3" />
+      <img src="/img/4.jpg" className="w-full h-auto object-cover" alt="Product 4" />
+      <img src="/img/5.jpg" className="w-full h-auto object-cover" alt="Product 5" />
+      <img src="/img/6.jpg" className="w-full h-auto object-cover" alt="Product 6" />
+      <img src="/img/7.jpg" className="w-full h-auto object-cover" alt="Product 7" />
+      <img src="/img/8.jpg" className="w-full h-auto object-cover" alt="Product 8" />
+      <img src="/img/9.jpg" className="w-full h-auto object-cover" alt="Product 9" />
+      <img src="/img/10.jpg" className="w-full h-auto object-cover" alt="Product 10" />
+    </div>
   );
 };
 
